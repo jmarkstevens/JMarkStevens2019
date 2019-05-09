bf153e953122bd1cee29e3c01e1804fb715afc4e
J. Mark Stevens <github@jmarkstevens.com>
2019-05-09 08:51:18 -0700

    home page

---
+++ src/components/home/home-text.js
@@ -0,0 +1,42 @@
+export default {
+  head1: `
+    J. Mark Stevens Consulting
+  `,
+  head2: `
+    React and React Native Developer
+  `,
+  text1: `
+    Seasoned full stack JavaScript developer with over 15 years of experience. 
+    Fluent in JavaScript(es6) and TypeScript. 
+    On the front end I have skilled to advanced knowledge of React, 
+    React Native, Angular 2+, redux, Apollo Client (graphql), material ui, 
+    fetch, websockets, html and css. On the back end I have skilled to advanced 
+    knowledge of Ubuntu, nginx, NodeJS (with yarn or npm), express, 
+    Apollo Server (graphql), mysql, sqlite, mongodb. I also use git, 
+    webpack, lodash, electron and a host of other npm libraries.
+  `,
+  text2: `
+    While writing clean and well organized code for ease of understanding and maintainability,
+    I deliver creative solutions that work.
+  `,
+  head01: `
+    Experienced Web/Desktop Software Developer and Consultant
+  `,
+  text01: `
+    Hi, my name is Mark Stevens and I have over 20 years of experience as a software developer/consultant.
+    I specialize in full stack data intensive line of business
+    and web applications. By full stack I mean the front end (UI) and the back end (database and web server).
+  `,
+  text02: `
+    My current development stack is Javascript, React or React-Native, HTML5, CSS3 and Node JS.
+    I have worked with data (design and query) from SQLite, MySql, SQL Server, PostgreSQL, MongoDB, Excel, EXIF (from picture files)
+    , JSON and restful sources.
+    My DevOps experience includes deploying Nginx, MongoDB and MySql on Ubuntu running multiple instances of Node JS.
+  `,
+  text03: `
+    I am very good at understanding business requirements and identifying ways to maximize technology benefits.
+  `,
+  text04: `
+    You can view my resume and examples at http://jmarkstevens.com.
+  `
+}
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,37 @@
+import classNames from 'classnames'
 import React from 'react'
 
+import HomeText from './home-text'
+
+const sec1ImgUrl = './img/162284b.jpg'
+
 const Home = () => {
-  return <div className="home-container">Home</div>
+  const homePageSty = {
+    backgroundImage: `url(${sec1ImgUrl})`,
+    backgroundSize: 'cover'
+  }
+
+  const HeadClass = classNames('home-text', 'home-text-heading')
+  const SubHeadClass = classNames('home-text', 'home-text-subhead')
+  return (
+    <div className="home-container" style={homePageSty}>
+      <div className="home-center">
+        <div className="home-inner-text">
+          <div className="home-inner-padding">
+            <img
+              alt="head shot"
+              className="home-inner-image"
+              src="img/jms04.jpg"
+            />
+            <div className={HeadClass}>{HomeText.head1}</div>
+            <div className={SubHeadClass}>{HomeText.head2}</div>
+            <div className="home-text">{HomeText.text1}</div>
+            <div className="home-text">{HomeText.text2}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
