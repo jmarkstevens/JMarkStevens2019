05b45aa434cb14036dcb40f63d83e0288c64e493
J. Mark Stevens <github@jmarkstevens.com>
2019-05-09 08:56:59 -0700

    resume page

--- src/components/resume/resume.jsx
+++ src/components/resume/resume.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
 const Resume = () => {
-  return <div className="resume-container">Resume</div>
+  let deviceType = 'mobile'
+  switch (window.navigator.platform) {
+    case 'MacIntel':
+    case 'Linux x86_64':
+    case 'Win32':
+      deviceType = 'desktop'
+      break
+    default:
+      break
+  }
+  let pdfArticle = null
+  if (deviceType === 'mobile') {
+    window.open('http://jmarkstevens.com/JMarkStevensResume.pdf', '_blank')
+  } else {
+    pdfArticle = (
+      <embed
+        width="100%"
+        height="100%"
+        src="JMarkStevensResume.pdf"
+        type="application/pdf"
+      />
+    )
+  }
+  return <div className="resume-container">{pdfArticle}</div>
 }
 
 export default Resume
